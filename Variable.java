8b07a4ae082ac434ea43f8be95eec78756dffc5f
Aryan Rathod <115866974+Aryanrathod21@users.noreply.github.com>
2024-08-08 19:51:36 +0530

    variables

---
+++ Variable.java
@@ -0,0 +1,12 @@
+public class Variable {
+    public static void main(String[] args) {
+        int a = 25;
+        int b = 10;
+        int sum = a + b;
+        int diff = a - b;
+        int multi = a * b;
+        System.out.println("The sum of two number is : "+sum);
+        System.out.println("The difference of two number is : "+diff);
+        System.out.println("The multiplication of two number is : "+multi);
+    }
+}
