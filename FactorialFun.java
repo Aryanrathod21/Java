d47934e65e4c0cb70c458758dc9f53484b0704e5
Aryan Rathod <115866974+Aryanrathod21@users.noreply.github.com>
2024-08-09 20:19:05 +0530

    Function/factorial

---
+++ FactorialFun.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class FactorialFun {
+    public static void printFactorial(int n){
+        if (n < 0) {
+            System.out.println("invalid number");
+            return;
+        };
+        int fact = 1;
+        for(int i = n;i>=1;i--){
+            fact = fact*i;
+        }
+        System.out.println("The factorial of the given number is : "+fact);
+
+        return;
+    }
+    public static void main(String[] args) {
+        System.out.println("Enter a number : ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        printFactorial(n);
+    }
+}
