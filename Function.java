6b8e617f0ba6975e181e676aa220cf8789a6e56a
Aryan Rathod <115866974+Aryanrathod21@users.noreply.github.com>
2024-08-09 20:07:04 +0530

    Function/additon

--- Function.java
+++ Function.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 public class Function {
     public static void printMyName(String name){
         System.out.println(name);
-        return;
     }
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String name = sc.next();
-        printMyName(name);
+        try (Scanner sc = new Scanner(System.in)) {
+            String name = sc.next();
+            printMyName(name);
+        }
     }
 }
