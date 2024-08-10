1d228bd593ae97eddcef62ed25b0c6051f5e3712
Aryan Rathod <115866974+Aryanrathod21@users.noreply.github.com>
2024-08-10 14:51:53 +0530

    Problem1/avg

---
+++ Avg.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Avg {
+    public static void Averege(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first number : ");
+        int a = sc.nextInt();
+        System.out.println("Enter the second number : ");
+        int b = sc.nextInt();
+        System.out.println("Enter the third number : ");
+        int c = sc.nextInt();
+
+        int avg = (a + b + c)/3;
+        System.out.println("The avg of three numbers is : "+avg);
+
+    }
+    public static void main(String[] args) {
+        Averege();
+    }
+}
--- FactorialFun.java
+++ FactorialFun.java
@@ -5,7 +5,7 @@ public class FactorialFun {
         if (n < 0) {
             System.out.println("invalid number");
             return;
-        };
+        }
         int fact = 1;
         for(int i = n;i>=1;i--){
             fact = fact*i;
